75740e2ef92f5b8212dc992aa131cc59586901c9
runup-dev <61175928+runup-dev@users.noreply.github.com>
2020-06-02 21:03:15 +0900

    Delete setClient.sql

--- setClient.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS wp_dowhastudio;
-CREATE USER IF NOT EXISTS dowhastudio_www@localhost;
-SET PASSWORD FOR dowhastudio_www@localhost= PASSWORD("dowhastudio@6951");
-GRANT ALL PRIVILEGES ON wp_dowhastudio.* TO dowhastudio_www@localhost IDENTIFIED BY "dowhastudio@6951";
-FLUSH PRIVILEGES;
